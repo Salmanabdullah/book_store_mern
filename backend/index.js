e914a5b2846cba3ce07c413c9b095588b26b5318
Salmanabdullah <salmandcc@gmail.com>
2024-01-30 23:48:42 +0100

    add frontend home functionality

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,14 @@ const app = express();
 app.use(express.json());
 
 // Middleware to handle cors policy
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors())
+//app.use(
+//  cors({
+//    origin: "http://localhost:3000",
+//    methods: ["GET", "POST", "PUT", "DELETE"],
+//    allowedHeaders: ["Content-Type"],
+//  })
+//);
 
 app.get("/", (req, res) => {
   console.log(req);
